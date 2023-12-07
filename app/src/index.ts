a83e6f7a7c6027eab5f42e7775d3b456d18b7b27
Yusuke Higuchi <hy2k@outlook.jp>
2023-12-07 12:51:07 +0900

    Add argument parsing

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,1 +1,47 @@
-console.log('Hello World!');
+import * as fs from 'fs/promises';
+
+import { program } from 'commander';
+
+interface CLIOptions {
+	readonly placePath: string;
+	readonly scriptPath: string;
+}
+
+program
+	.name('run-in-roblox-studio')
+	.requiredOption('-script, --script-path <file.lua>', 'path to the script to run in Roblox Studio')
+	.option('-place, --place-path <file.rbxl>', '(optional) path to the place file to run in Roblox Studio')
+	.showHelpAfterError();
+
+class FsError extends Error {}
+
+async function validateOptions(options: CLIOptions) {
+	if (options.placePath) {
+		try {
+			await fs.access(options.placePath, fs.constants.R_OK);
+		} catch (err) {
+			throw new FsError(`Unable to read place file: ${options.placePath}`, {
+				cause: err,
+			});
+		}
+	}
+}
+
+async function run() {
+	program.parse();
+
+	const options: CLIOptions = program.opts();
+
+	await validateOptions(options);
+
+	console.log(options);
+}
+
+run()
+	.then(() => {
+		process.exit();
+	})
+	.catch((err) => {
+		console.error(err);
+		process.exit(1);
+	});
