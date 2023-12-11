d0d5534fd2fdd3e66c5068f55b7a046c807eae48
Yusuke Higuchi <hy2k@outlook.jp>
2023-12-11 15:05:20 +0900

    Fix watch script
    Use Rojo build with --watch flag
    
    Fix

--- packages/plugin/scripts/watch-plugin.mjs
+++ packages/plugin/scripts/watch-plugin.mjs
@@ -1,58 +1,20 @@
 // @ts-check
 
-// Workaround for rojo build --plugin not working from WSL
+// Cross platform watch script for rojo plugins
 
-import fs from 'node:fs/promises';
+import { $ } from 'execa';
 import path from 'node:path';
 import { getRobloxStudioPath } from 'roblox-studio-pathutil';
+
+const $$ = $({
+	stdio: 'inherit',
+	verbose: true,
+});
 
 const ENV_VAR_ROBLOX_STUDIO_PATH = 'ROBLOX_STUDIO_PATH';
 const DEV_PLUGIN_NAME = 'run-in-roblox-studio-dev.rbxm';
 
-const dirname = path.dirname(new URL(import.meta.url).pathname);
-const pluginPath = path.resolve(dirname, '..', DEV_PLUGIN_NAME);
-
 const robloxStudioRoot = process.env[ENV_VAR_ROBLOX_STUDIO_PATH];
 const robloxStudioPath = await getRobloxStudioPath(robloxStudioRoot ? path.resolve(robloxStudioRoot) : undefined);
 
-const ac = new AbortController();
-const { signal } = ac;
-
-process.on('SIGINT', () => {
-	ac.abort();
-	console.log('Stopped watching file.');
-	process.exit(0);
-});
-
-let debounce = false;
-setInterval(() => {
-	debounce = false;
-}, 1000);
-
-(async () => {
-	try {
-		const watcher = fs.watch(pluginPath, { signal: signal });
-		console.log(`Watching ${DEV_PLUGIN_NAME} for changes...`);
-
-		for await (const _event of watcher) {
-			if (debounce) {
-				continue;
-			}
-
-			try {
-				await fs.copyFile(pluginPath, path.join(robloxStudioPath.plugins, DEV_PLUGIN_NAME));
-				console.log(`️Copied ${DEV_PLUGIN_NAME} to Roblox Plugins folder.`);
-			} catch (err) {
-				console.error(`️Failed to copy ${DEV_PLUGIN_NAME}`);
-				console.error(err);
-			} finally {
-				debounce = true;
-			}
-		}
-	} catch (err) {
-		if (err.name === 'AbortError') {
-			return;
-		}
-		throw err;
-	}
-})();
+$$`rojo build -o ${path.join(robloxStudioPath.plugins, DEV_PLUGIN_NAME)} --watch`;
