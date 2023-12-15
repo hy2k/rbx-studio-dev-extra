82dee61a2fb51d5c8c2faaaf98c6ebfdf523321e
Yusuke Higuchi <hy2k@outlook.jp>
2023-12-15 18:39:47 +0900

    Nest plugin package inside

--- eslint.config.js
+++ eslint.config.js
@@ -4,22 +4,210 @@ import typescriptParser from '@typescript-eslint/parser';
 import importPlugin from 'eslint-plugin-import';
 import perfectionistPlugin from 'eslint-plugin-perfectionist';
 import robloxTsPlugin from 'eslint-plugin-roblox-ts';
-import path from 'node:path';
+import path from 'path';
 
 const ignores = ['**/node_modules/*', '**/dist/**', '**/out/**'];
 
-const robloxTsPath = './packages/plugins';
+/** @type {import("eslint").Linter.FlatConfig} */
+const baseConfig = {
+	files: ['**/*.ts', '**/*.js', '**/*.mjs'],
+	ignores: ignores,
+	plugins: {
+		perfectionist: perfectionistPlugin,
+	},
+	rules: {
+		...eslint.configs.recommended.rules,
+
+		curly: ['warn', 'all'],
+		'function-paren-newline': ['warn', 'multiline-arguments'],
+		'object-shorthand': ['warn', 'never'],
+
+		'perfectionist/sort-array-includes': [
+			'warn',
+			{
+				order: 'asc',
+				'spread-last': true,
+				type: 'natural',
+			},
+		],
+
+		'perfectionist/sort-classes': [
+			'warn',
+			{
+				groups: [
+					'index-signature',
+					'static-property',
+					'private-property',
+					'property',
+					'constructor',
+					'static-method',
+					'private-method',
+					'static-private-method',
+					'method',
+					['get-method', 'set-method'],
+					'unknown',
+				],
+				order: 'asc',
+				type: 'natural',
+			},
+		],
+
+		'perfectionist/sort-enums': [
+			'warn',
+			{
+				order: 'asc',
+				type: 'natural',
+			},
+		],
+		'perfectionist/sort-exports': [
+			'warn',
+			{
+				order: 'asc',
+				type: 'natural',
+			},
+		],
+		'perfectionist/sort-imports': [
+			'warn',
+			{
+				groups: [
+					'type',
+					['builtin', 'external'],
+					'internal-type',
+					'internal',
+					['parent-type', 'sibling-type', 'index-type'],
+					['parent', 'sibling', 'index'],
+					'object',
+					'unknown',
+				],
+				'newlines-between': 'always',
+				order: 'asc',
+				type: 'natural',
+			},
+		],
+		'perfectionist/sort-interfaces': [
+			'warn',
+			{
+				order: 'asc',
+				type: 'natural',
+			},
+		],
+		'perfectionist/sort-jsx-props': [
+			'warn',
+			{
+				order: 'asc',
+				type: 'natural',
+			},
+		],
+		'perfectionist/sort-maps': [
+			'warn',
+			{
+				order: 'asc',
+				type: 'natural',
+			},
+		],
+		'perfectionist/sort-named-exports': [
+			'warn',
+			{
+				order: 'asc',
+				type: 'natural',
+			},
+		],
+		'perfectionist/sort-named-imports': [
+			'warn',
+			{
+				order: 'asc',
+				type: 'natural',
+			},
+		],
+		'perfectionist/sort-object-types': [
+			'warn',
+			{
+				order: 'asc',
+				type: 'natural',
+			},
+		],
+		'perfectionist/sort-objects': [
+			'warn',
+			{
+				order: 'asc',
+				type: 'natural',
+			},
+		],
+		'perfectionist/sort-union-types': [
+			'warn',
+			{
+				order: 'asc',
+				type: 'natural',
+			},
+		],
+		'prefer-arrow-callback': 'error',
+	},
+};
+
+const typescriptRules = {
+	...typescriptPlugin.configs.recommended.rules,
+
+	'@typescript-eslint/consistent-indexed-object-style': ['warn', 'index-signature'],
+	'@typescript-eslint/consistent-type-imports': [
+		'warn',
+		{
+			disallowTypeAnnotations: true,
+			fixStyle: 'separate-type-imports',
+			prefer: 'type-imports',
+		},
+	],
+
+	'@typescript-eslint/indent': 'off',
+	'@typescript-eslint/method-signature-style': ['warn', 'property'],
+	'@typescript-eslint/no-shadow': 'error',
+
+	'@typescript-eslint/no-unused-vars': [
+		'warn',
+		{
+			args: 'all',
+			argsIgnorePattern: '^_',
+			varsIgnorePattern: '^_',
+		},
+	],
+
+	'@typescript-eslint/switch-exhaustiveness-check': 'error',
+	// Perf
+	indent: 'off',
+
+	'no-shadow': 'off',
+
+	'no-unused-vars': 'off',
+};
+
+/** @type {import("eslint").Linter.FlatConfig} */
+const typescriptConfig = {
+	files: ['**/*.ts'],
+	ignores: ignores,
+	languageOptions: {
+		parser: typescriptParser,
+		parserOptions: {
+			project: path.resolve('./tsconfig.json'),
+			sourceType: 'module',
+		},
+	},
+	plugins: {
+		'@typescript-eslint': typescriptPlugin,
+	},
+	rules: typescriptRules,
+};
 
 /** @type  {import("eslint").Linter.FlatConfig} */
 const robloxTsConfig = {
-	files: [path.resolve(robloxTsPath, '**/*.ts')],
+	files: ['**/plugin/**/*.ts'],
+	ignores: ignores,
 	languageOptions: {
 		parserOptions: {
 			ecmaFeatures: {
 				jsx: true,
 			},
 			ecmaVersion: 2018,
-			project: path.resolve(robloxTsPath, 'tsconfig.json'),
+			// project: path.resolve(robloxTsPath, 'tsconfig.json'),
+			project: '**/plugin/tsconfig.json',
 			sourceType: 'module',
 		},
 	},
@@ -28,8 +216,8 @@ const robloxTsConfig = {
 		'roblox-ts': robloxTsPlugin,
 	},
 	rules: {
-		...typescriptPlugin.configs.recommended.rules,
 		...robloxTsPlugin.configs.recommended.rules,
+		...typescriptRules,
 
 		// Roblox-ts does not allow this
 		'@typescript-eslint/ban-ts-comment': 'error',
@@ -39,7 +227,7 @@ const robloxTsConfig = {
 	},
 };
 
-/** @type  {import("eslint").Linter.FlatConfig} */
+/** @type {import("eslint").Linter.FlatConfig} */
 const importConfig = {
 	files: ['**/*.ts'],
 	ignores: ignores,
@@ -61,209 +249,17 @@ const importConfig = {
 		'import/no-mutable-exports': 'warn',
 	},
 };
-
-/** @type  {import("eslint").Linter.RulesRecord} */
-const perfectionistRules = {
-	'perfectionist/sort-array-includes': [
-		'warn',
-		{
-			order: 'asc',
-			'spread-last': true,
-			type: 'natural',
-		},
-	],
-
-	'perfectionist/sort-classes': [
-		'warn',
-		{
-			groups: [
-				'index-signature',
-				'static-property',
-				'private-property',
-				'property',
-				'constructor',
-				'static-method',
-				'private-method',
-				'static-private-method',
-				'method',
-				['get-method', 'set-method'],
-				'unknown',
-			],
-			order: 'asc',
-			type: 'natural',
-		},
-	],
-	'perfectionist/sort-enums': [
-		'warn',
-		{
-			order: 'asc',
-			type: 'natural',
-		},
-	],
-	'perfectionist/sort-exports': [
-		'warn',
-		{
-			order: 'asc',
-			type: 'natural',
-		},
-	],
-	'perfectionist/sort-imports': [
-		'warn',
-		{
-			groups: [
-				'type',
-				['builtin', 'external'],
-				'internal-type',
-				'internal',
-				['parent-type', 'sibling-type', 'index-type'],
-				['parent', 'sibling', 'index'],
-				'object',
-				'unknown',
-			],
-			'newlines-between': 'always',
-			order: 'asc',
-			type: 'natural',
-		},
-	],
-	'perfectionist/sort-interfaces': [
-		'warn',
-		{
-			order: 'asc',
-			type: 'natural',
-		},
-	],
-	'perfectionist/sort-jsx-props': [
-		'warn',
-		{
-			order: 'asc',
-			type: 'natural',
-		},
-	],
-	'perfectionist/sort-maps': [
-		'warn',
-		{
-			order: 'asc',
-			type: 'natural',
-		},
-	],
-	'perfectionist/sort-named-exports': [
-		'warn',
-		{
-			order: 'asc',
-			type: 'natural',
-		},
-	],
-	'perfectionist/sort-named-imports': [
-		'warn',
-		{
-			order: 'asc',
-			type: 'natural',
-		},
-	],
-	'perfectionist/sort-object-types': [
-		'warn',
-		{
-			order: 'asc',
-			type: 'natural',
-		},
-	],
-	'perfectionist/sort-objects': [
-		'warn',
-		{
-			order: 'asc',
-			type: 'natural',
-		},
-	],
-	'perfectionist/sort-union-types': [
-		'warn',
-		{
-			order: 'asc',
-			type: 'natural',
-		},
-	],
-};
-
 /** @type {import("eslint").Linter.FlatConfig[]} */
 export default [
-	{
-		files: ['**/*.ts', '**/*.js', '**/*.mjs'],
-		ignores: ignores,
-		plugins: {
-			perfectionist: perfectionistPlugin,
-		},
-		rules: {
-			...eslint.configs.recommended.rules,
+	baseConfig,
 
-			curly: ['warn', 'all'],
-			'function-paren-newline': ['warn', 'multiline-arguments'],
-			'object-shorthand': ['warn', 'never'],
-			'prefer-arrow-callback': 'error',
-
-			...perfectionistRules,
-		},
-	},
+	typescriptConfig,
+	robloxTsConfig,
 
 	// Disable rules that are incompatible with or better handled by TypeScript
 	{
 		rules: typescriptPlugin.configs['eslint-recommended'].overrides[0].rules,
 	},
 
-	// Enable TypeScript-specific rules
-	{
-		languageOptions: {
-			parser: typescriptParser,
-			parserOptions: {
-				project: path.resolve('./tsconfig.json'),
-				sourceType: 'module',
-			},
-		},
-		plugins: {
-			'@typescript-eslint': typescriptPlugin,
-		},
-		rules: {
-			...typescriptPlugin.configs.recommended.rules,
-
-			'@typescript-eslint/consistent-indexed-object-style': ['warn', 'index-signature'],
-			'@typescript-eslint/consistent-type-imports': [
-				'warn',
-				{
-					disallowTypeAnnotations: true,
-					fixStyle: 'separate-type-imports',
-					prefer: 'type-imports',
-				},
-			],
-
-			'@typescript-eslint/indent': 'off',
-			'@typescript-eslint/method-signature-style': ['warn', 'property'],
-			'@typescript-eslint/no-shadow': 'error',
-
-			'@typescript-eslint/no-unused-vars': [
-				'warn',
-				{
-					args: 'all',
-					argsIgnorePattern: '^_',
-					varsIgnorePattern: '^_',
-				},
-			],
-			'@typescript-eslint/no-unused-vars': [
-				'warn',
-				{
-					args: 'all',
-					argsIgnorePattern: '^_',
-					varsIgnorePattern: '^_',
-				},
-			],
-
-			'@typescript-eslint/switch-exhaustiveness-check': 'error',
-			// Perf
-			indent: 'off',
-
-			'no-shadow': 'off',
-
-			'no-unused-vars': 'off',
-		},
-	},
-
 	importConfig,
-	robloxTsConfig,
 ];
